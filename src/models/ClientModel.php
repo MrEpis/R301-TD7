fcc2b2da08e3c8d74f3e993c3440e6be9a9b1021
MrEpis <jesaispas2006@gmail.com>
2025-12-30 15:42:47 +0100

    Ajout connexion db et modele client

--- src/models/ClientModel.php
+++ src/models/ClientModel.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once "../config/Database.php";
+
 class ClientModel
 {
+    private PDO $db;
+
+    private function __construct(){
+        $this->db = Database::getInstance();
+    }
+
+    public function dbFindUser($email){
+        $query = $this->db->prepare("SELECT * FROM client WHERE email = :email");
+        $query->execute(array(
+            'email' => $email
+        ));
+        return $query->fetch();
+    }
+
+    public function dbCreateUser($email, $passwordHash, $name, $firstname): bool
+    {
+        $query = $this->db->prepare("INSERT INTO client (email, mot_de_passe, nom, prenom) VALUES (:email, :mot_de_passe, :nom, :prenom)");
+        return $query->execute(array(
+            'email' => $email,
+            'mot_de_passe' => $passwordHash,
+            'nom' => $name,
+            'prenom' => $firstname
+        ));
+    }
+
 
 }
